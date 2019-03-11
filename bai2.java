4f4399b48f8bbd99033154f121a078ec6f761595
manly10x <48342143+manly10x@users.noreply.github.com>
2019-03-11 08:27:16 +0700

    Add files via upload

---
+++ bai2.java
@@ -0,0 +1,24 @@
+package session1;
+
+public class bai2 {
+    public static void main(String [] args){
+        int n;
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("moi ban nhap mot so tu ban phim :");
+        n=scanner.nextInt();
+        for (int i=0;i<n;i++)
+            System.out.println(n+"so dau trong day Fibonacci : "+fibonacci(i));
+    }
+    static long fibonacci(int n){
+        long f0=0, f1=1,fn=0;
+        if ( n==1) fn=n;
+        else for (int i=2;i<=n;i++)
+        {
+            fn=f0+f1;
+            f0=f1;
+            f1=fn;
+        }
+        return fn;
+    }
+}
+}
