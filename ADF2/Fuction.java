0ad8fac25936290ee2215453dd2245002bac197b
manly10x <48342143+manly10x@users.noreply.github.com>
2019-05-06 10:26:25 +0700

    Add files via upload

---
+++ ADF2/Fuction.java
@@ -0,0 +1,43 @@
+package ADF2;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Fuction {
+    //1. Add function
+    public void add (){
+        //Nhap du lieu tu man hinh
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Book ID:");
+        String id = scanner.nextLine();
+        System.out.println("Book Title:");
+        String title = scanner.nextLine();
+        System.out.println("Author:");
+        String author = scanner.nextLine();
+        System.out.println("Price:");
+        Integer price = scanner.nextInt();
+
+        Book newBook = new Book(id,title,author,price);
+        Bookrecords bookrecords = new Bookrecords();
+        bookrecords.add(newBook);
+    }
+    //2.Save fuction
+    public void save(){
+
+    }
+
+    //3.Display function
+    public void display(){
+        Connector connector = Connector.getInstance();
+        Bookrecords bookrecords = new Bookrecords();
+        ArrayList<Book> listBook= bookrecords.getList();
+        for(Book x: listBook){
+            System.out.println("Book ID:"+ x.getId());
+            System.out.println("Book Title:"+ x.getTitle());
+            System.out.println("Author:"+ x.getAuthor());
+            System.out.println("Price:"+ x.getPrice());
+            System.out.println();
+        }
+
+    }
+}
---
+++ ADF2/Interface.java
@@ -0,0 +1,12 @@
+package ADF2;
+
+
+
+import java.util.ArrayList;
+
+public interface Interface<E>  {
+    ArrayList<E> getList();
+    boolean add(E e);
+    // boolean update(E e);
+    // boolean delete(E e);
+}
